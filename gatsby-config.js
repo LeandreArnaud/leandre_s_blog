7287ab9256b3f381ef12da312473ad2d219a7761
LeandreArnaud <pro.leandre.arnaud@gmail.com>
2021-07-23 17:20:29 +0200

    updating website icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,19 @@ module.exports = {
     // images:
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`],
+    `gatsby-transformer-sharp`,
+
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: 'src/images/icon.jpg'
+      },
+    },
+  ]
 };
