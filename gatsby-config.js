ba4956470ffc48599016d944541c43d188b7e37d
LeandreArnaud <pro.leandre.arnaud@gmail.com>
2021-07-23 16:58:42 +0200

    Push background img and gatsby-plugin-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,11 @@ module.exports = {
   siteMetadata: {
     title: "leandre_s_blog",
   },
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    // scroll parallax:
+    `gatsby-plugin-sass`,
+    // images:
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,62 @@
 import React from "react"
 import Layout from "../components/layout"
+import Fade from 'react-reveal/Fade';
+import { ParallaxProvider, ParallaxBanner } from 'react-scroll-parallax';
+import { StaticImage } from "gatsby-plugin-image"
+
 
 const IndexPage = () => (
-  <Layout>
-    <div className='homeContainer'>
-      <img src='static/background.JPG' alt='background'>
-      </img>
-
-
-      <ul className='homeMenuContainer'>
-        <li>Resume</li>
-        <li>Blog</li>
-        <li>Projects</li>
-      </ul>
-
-      <div className='homeMainTitleContainer'>
-        <h1>Léandre ARNAUD</h1>
-        <h3>Creates its little blog</h3>
+  <ParallaxProvider>
+    <Layout>
+      <div>
+        <ParallaxBanner
+          className="homeContainer"
+          layers={[
+              {
+                children: <StaticImage src="../images/background.JPG" alt="background" className="homeBackgroundImg" />,
+                amount: 0,
+              },
+              {
+                  children:         <Fade top cascade>
+                                        <ul className='homeMenuContainer'>
+                                        <li>Resume</li>
+                                        <li>Blog</li>
+                                        <li>Projects</li>
+                                      </ul>
+                                    </Fade>,
+                  amount: 0.2,
+              },
+              {
+                  children: <Fade top cascade>
+                              <div className='homeMainTitleContainer'>
+                                <h1>Blablou Bliblou</h1>
+                                <h3>Creates its little blog</h3>
+                              </div>
+                            </Fade>,
+                  amount: 0.3,
+              },
+          ]}
+          style={{
+            height: '100vh',
+          }}>
+        </ParallaxBanner>
       </div>
-    </div>
-  </Layout>
+
+      <div>
+        <h1>BLABLOU</h1>
+        <h1>BLABLOU</h1>
+        <h1>BLABLOU</h1>
+        <h1>BLABLOU</h1>
+        <h1>BLABLOU</h1>
+        <h1>BLABLOU</h1>
+        <h1>BLABLOU</h1>
+        <h1>BLABLOU</h1>
+        <h1>BLABLOU</h1>
+        <h1>BLABLOU</h1>
+        <h1>BLABLOU</h1>
+      </div>
+    </Layout>
+  </ParallaxProvider>
 )
 
 export default IndexPage
